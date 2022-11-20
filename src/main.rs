9e90a7369db846ab7edc5f6bbcb561cb58aa597b
isatispro <isatispro@qq.com>
2022-11-20 16:51:04 +0800

    hello world

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,32 @@
+// os/src/main.rs
 #![no_std]
 #![no_main]
+#![feature(panic_info_message)]
 
+// use log::*;
+
+#[macro_use]
+mod console;
 mod lang_item;
+// mod logging;
+mod sbi;
 
-use core::arch::global_asm;
+// use core::{arch::global_asm, panicking::panic};
 
-global_asm!(include_str!("entry.asm"));
-// fn main() {
-//     // println!("Hello, world!");
-// }
+core::arch::global_asm!(include_str!("entry.asm"));
+
+#[no_mangle]
+pub fn rust_main() -> ! {
+    clear_bss();
+    println!("Hello world!");
+    panic!("Shutdown machine!");
+}
+
+fn clear_bss() {
+    extern "C" {
+        fn sbss();
+        fn ebss();
+    }
+
+    (sbss as usize..ebss as usize).for_each(|a| unsafe { (a as *mut u8).write_volatile(0) })
+}
