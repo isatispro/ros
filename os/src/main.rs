43659d201f3908f57b7f1954ad61095e6f7f8b74
isatispro <isatispro@qq.com>
2022-11-29 23:38:29 +0800

    multi process

--- os/src/main.rs
+++ os/src/main.rs
@@ -1,25 +1,33 @@
-// os/src/main.rs
+// main.rs
+// #![deny(missing_docs)]
+#![deny(warnings)]
 #![no_std]
 #![no_main]
 #![feature(panic_info_message)]
 
-// use log::*;
+use core::arch::global_asm;
 
 #[macro_use]
 mod console;
 mod lang_item;
-// mod logging;
 mod sbi;
+mod sync;
 
-// use core::{arch::global_asm, panicking::panic};
+pub mod batch;
+pub mod syscall;
+pub mod trap;
 
 core::arch::global_asm!(include_str!("entry.asm"));
+global_asm!(include_str!("link_app.S"));
 
 #[no_mangle]
 pub fn rust_main() -> ! {
     clear_bss();
     println!("Hello Ros!");
-    panic!("shutdown Ros!");
+
+    trap::init();
+    batch::init();
+    batch::run_next_app();
 }
 
 fn clear_bss() {
